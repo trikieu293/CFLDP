997cc92920e84018ab131c649c35e828f7da475b
Kieu Minh Tri <kieu01@ads.uni-passau.de>
2024-06-25 00:27:03 +0200

    adding exact algorithm

--- main.py
+++ main.py
@@ -7,18 +7,15 @@ import math
 import time
 import itertools
 
-random.seed(123)
-
 def cfldp(n_customer, alpha, beta, lamda, theta):
+    random.seed(123)
     MAP_SIZE = 1000
     CUSTOMERS = n_customer
-    BUDGET = 20
     ATTRACTIVENESS_ATTRIBUTES = 2
 
     POTENTIAL_LOCATION = CUSTOMERS // 3
     EXISTING_COMPETITIVE_FACILITIES = POTENTIAL_LOCATION // 3
     AVAILABLE_LOCATIONS = POTENTIAL_LOCATION - EXISTING_COMPETITIVE_FACILITIES
-
 
     ALPHA = alpha            # approximation level
     BETA = beta              # the distance sensitivity parameter
@@ -58,7 +55,7 @@ def cfldp(n_customer, alpha, beta, lamda, theta):
     # creating dict of scenarios
     scenarios_attr = {}
     for k in range(1, ATTRACTIVENESS_ATTRIBUTES + 1):
-        value = 3
+        value = 2
         scenarios_attr.update({k: [level for level in range(value)]})
 
     product = itertools.product(*[k for k in scenarios_attr.values()])
@@ -73,9 +70,9 @@ def cfldp(n_customer, alpha, beta, lamda, theta):
     for i in N:
         w.update({i: random.randint(1, 5)})
     # initiating attractiveness for competitive facilities
-    C_attractiveness = {}
+    c_attractiveness = {}
     for c in C:
-        C_attractiveness.update({c: random.randint(1, 10)})
+        c_attractiveness.update({c: random.randint(5, 10)})
 
     ### Help functions
     def get_attractiveness(scenario):
@@ -85,7 +82,7 @@ def cfldp(n_customer, alpha, beta, lamda, theta):
             attractiveness = attractiveness*((1 + i)**THETA)
         return attractiveness
     def get_cost(scenario):
-        return 1 + 0.5 * sum(R.get(scenario))
+        return 1 + 1 * sum(R.get(scenario))
 
     def get_utility(customer, facility, scenario):
         return get_attractiveness(scenario) * (distances.get((customer, facility)) + 1) ** (-BETA)
@@ -103,7 +100,7 @@ def cfldp(n_customer, alpha, beta, lamda, theta):
     def get_u_c(customer):
         utility_sum = 0
         for c in C:
-            utility_sum += C_attractiveness.get(c) * (distances.get((customer, c)) + 1) ** (-BETA)
+            utility_sum += c_attractiveness.get(c) * (distances.get((customer, c)) + 1) ** (-BETA)
         return utility_sum
 
     def get_max_u_s(customer):
@@ -149,8 +146,8 @@ def cfldp(n_customer, alpha, beta, lamda, theta):
                 low = midpoint
             else:
                 high = midpoint
-        if midpoint >= (1 - 0.0001) * temp:
-            return temp
+        # if midpoint >= (1 - 0.0001) * temp:
+        #     return temp
         return midpoint
 
     def data_callback(model, where):
@@ -263,6 +260,130 @@ def cfldp(n_customer, alpha, beta, lamda, theta):
 
     return [x_result, y_result, a_dict, b_dict, c_dict, l_dict]
 
+def exact(n_customer, beta, lamda, theta):
+    random.seed(123)
+    MAP_SIZE = 1000
+    CUSTOMERS = n_customer
+    ATTRACTIVENESS_ATTRIBUTES = 2
+
+    POTENTIAL_LOCATION = CUSTOMERS // 3
+    EXISTING_COMPETITIVE_FACILITIES = POTENTIAL_LOCATION // 3
+    AVAILABLE_LOCATIONS = POTENTIAL_LOCATION - EXISTING_COMPETITIVE_FACILITIES
+
+    BETA = beta              # the distance sensitivity parameter
+    LAMBDA = lamda           # the elasticity parameter
+    THETA = theta            # sensitivity parameter of the utility function
+
+    N = [node for node in range(1, CUSTOMERS + 1)]          # index of customers
+    P = random.sample(N, POTENTIAL_LOCATION)                # index of potential locations
+    C = random.sample(P, EXISTING_COMPETITIVE_FACILITIES)   # index of competitive facility locations
+    S = [facility for facility in P if facility not in C]   # index of controlled facilities
+
+    # initiating locations for nodes
+    locations = {}
+    for i in N:
+        x = random.randint(1, MAP_SIZE - 1)
+        y = random.randint(1, MAP_SIZE - 1)
+
+        while (x, y) in locations.values():
+            x = random.randint(1, MAP_SIZE - 1)
+            y = random.randint(1, MAP_SIZE - 1)
+        locations.update({i: (x, y)})
+    print("Locations: "+str(locations))
+
+    # calculating distances between nodes
+    distances = {}
+    for i in N:
+        for j in N:
+            if i == j:
+                distances.update({(i, j): 0.1})
+            else:
+                x1, y1 = locations.get(i)
+                x2, y2 = locations.get(j)
+                distance = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+                distances.update({(i, j): distance})
+    print("Distances: "+str(distances))
+
+    # creating dict of scenarios
+    scenarios_attr = {}
+    for k in range(1, ATTRACTIVENESS_ATTRIBUTES + 1):
+        value = 2
+        scenarios_attr.update({k: [level for level in range(value)]})
+
+    product = itertools.product(*[k for k in scenarios_attr.values()])
+    R = {}
+    count = 1
+    for item in product:
+        R.update({count: list(item)})
+        count += 1
+
+    # creating dict of nodes' weight
+    w = {}
+    for i in N:
+        w.update({i: random.randint(1, 5)})
+    # initiating attractiveness for competitive facilities
+    c_attractiveness = {}
+    for c in C:
+        c_attractiveness.update({c: random.randint(5, 10)})
+
+    ### Help functions
+    def get_attractiveness(scenario):
+        # return 1 + 1 * sum(R.get(scenario))
+        attractiveness = 1.0
+        for i in R.get(scenario):
+            attractiveness = attractiveness*((1 + i)**THETA)
+        return attractiveness
+    def get_cost(scenario):
+        return 1 + 1 * sum(R.get(scenario))
+
+    def get_utility(customer, facility, scenario):
+        return get_attractiveness(scenario) * (distances.get((customer, facility)) + 1) ** (-BETA)
+
+    def get_u_c(customer):
+        utility_sum = 0
+        for c in C:
+            utility_sum += c_attractiveness.get(c) * (distances.get((customer, c)) + 1) ** (-BETA)
+        return utility_sum
+
+    ### Model
+    model = gp.Model()
+    x_index = [(j, r) for j in S for r in R.keys()]
+
+    # Decision varibales
+    x = model.addVars(x_index, vtype=GRB.INTEGER, name='x')
+    u1 = model.addVars([i for i in N], vtype=GRB.CONTINUOUS, name='u1')
+    u2 = model.addVars([i for i in N], vtype=GRB.CONTINUOUS, name='u2')
+    u3 = model.addVars([i for i in N], vtype=GRB.CONTINUOUS, name='u3')
+    u4 = model.addVars([i for i in N], vtype=GRB.CONTINUOUS, name='u4')
+    # Objective Function
+    model.setObjective(sum(w[i] * (1 - u4[i]) * u3[i] for i in N), GRB.MAXIMIZE)
+
+    # Constraints
+    for j in S:
+        model.addConstr(sum(x[j, r] for r in R.keys()) <= 1, name="Constraints 1")
+
+    model.addConstr(sum(sum(get_cost(r) * x[j, r] for r in R.keys()) for j in S) <= (AVAILABLE_LOCATIONS // 2) * get_cost(max(R.keys())), name="Constraints 2")
+    for i in N:
+        model.addConstr(u1[i] == get_u_c(i) + sum(x[j, r] * get_utility(i, j, r) for j in S for r in R.keys()), name="ConstrU1")
+        model.addConstr(u1[i] * u2[i] == 1.0, name="ConstrU2")
+        model.addConstr(u3[i] == 1 - get_u_c(i) * u2[i], name="ConstrU3")
+        e_lambda = math.exp(-LAMBDA)
+        model.addGenConstrExpA(u3[i], u4[i], e_lambda, name="ConstrU4")
+
+    model.Params.TimeLimit = 60*60
+    model.update()
+
+    model.optimize()
+
+    ### checking result
+    x_result = pd.DataFrame(x.keys(), columns=["j", "r"])
+    x_result["value"] = model.getAttr("X", x).values()
+    x_result.drop(x_result[x_result.value < 0.9].index, inplace=True)
+    x_result["attractiveness"] = [get_attractiveness(r) for r in x_result["r"]]
+
+    return [x_result]
 
 if __name__ == "__main__":
-    result = cfldp(40, 0.001, 1, 0.5, 0.9)
+    result_approximation = cfldp(40, 0.001, 1, 1, 1)
+    result_exact = exact(40, 1, 1, 1)
+    print(result_approximation, result_exact)
